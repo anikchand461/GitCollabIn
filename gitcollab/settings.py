bf713f53977ab7163ff4b52c9031521283030f10
Anik Chand <anikchand461@gmail.com>
2025-04-19 22:51:07 +0530

    .

--- gitcollab/settings.py
+++ gitcollab/settings.py
@@ -6,6 +6,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
